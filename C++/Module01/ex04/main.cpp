c250463e63396313854c1f62ab5d3172063450b4
Min guk Kang <minguk.gaang@gmail.com>
2022-02-26 16:45:57 +0100

    Module01-ex04: Started

--- C++/Module01/ex04/main.cpp
+++ C++/Module01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: min-kang <minguk.gaang@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 23:08:20 by min-kang          #+#    #+#             */
-/*   Updated: 2022/02/26 15:46:28 by min-kang         ###   ########.fr       */
+/*   Updated: 2022/02/26 16:45:23 by min-kang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,24 @@
 #include <string>
 
 int main(void) {
-	std::ifstream input("test");
-	std::ofstream output("test.replace");
+	std::ifstream input;
+	std::ofstream output;
 	std::string s1 = "lol", s2 = "yo";
 	
-	std::cout << s1 << "-------" << s2 << std::endl;
-
+	input.open("test");
+	if (!input) {
+		std::cout << "Cannot read the file" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	std::string r;
+	size_t	i;
+	while (getline(input, r)) {
+		if (i = find(r, s1))
+		
+			output << r;
+			
+	}
+	input.close();
+	output.close();
+	return (0);
 }
