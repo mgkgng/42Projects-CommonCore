5fe203535380436e8a555b6059991864dcb741bb
Min guk Kang <minguk.gaang@gmail.com>
2022-02-27 19:26:15 +0100

    Module01-ex05: some modifications

--- C++/Module01/ex05/Karen.cpp
+++ C++/Module01/ex05/Karen.cpp
@@ -6,11 +6,27 @@
 /*   By: min-kang <minguk.gaang@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 18:44:00 by min-kang          #+#    #+#             */
-/*   Updated: 2022/02/26 18:57:31 by min-kang         ###   ########.fr       */
+/*   Updated: 2022/02/27 16:13:56 by min-kang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Karen.hpp"
+
+Karen::Karen(void) {
+	return ;
+}
+
+Karen::~Karen(void) {
+	return ;
+}
+
+void Karen::complain(std::string level) {
+	switch (level)
+	{
+		case "DEBUG":
+		case ""
+	}
+}
 
 void Karen::debug(void) {
 	std::cout << "I'm debugging, bitch." << std::endl;
