ae8664cfead3950d5e418868aeaec42ca526654d
Minguk Gaang <minguk.gaang@gmail.com>
2022-03-16 13:56:34 +0100

    minishell: parenthis seg fault fixed

--- 09 Minishell(beforeMerge)/srcs/lexer/error.c
+++ 09 Minishell(beforeMerge)/srcs/lexer/error.c
@@ -6,7 +6,7 @@
 /*   By: min-kang <minguk.gaang@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 21:36:08 by min-kang          #+#    #+#             */
-/*   Updated: 2022/03/16 13:02:51 by min-kang         ###   ########.fr       */
+/*   Updated: 2022/03/16 13:56:09 by min-kang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,21 +25,20 @@ static int  p_check(t_token *tokens)
             p_count--;
         tokens = tokens->next;
     }
-    if (!p_count)
-        return (0);
-    return (1);
+    return (p_count);
 }
 
 static int check_error(t_token *tokens)
 {
+    if (p_check(tokens))
+		return (1);
 	while (tokens)
 	{
-		if (p_check(tokens))
-			return (1);
-		if ((tokens->token >= PIPE && tokens->token <= AND)
+		if ((tokens->token >= PIPE && tokens->token <= AND) && tokens->next
 			&& (tokens->next->token >= PIPE && tokens->next->token <= AND))
 			return (2);
-		if ((tokens->token == P_CLOSE) && tokens->next->token == ARG)
+		if ((tokens->token == P_CLOSE) && tokens->next
+            && tokens->next->token == ARG)
 			return (3);
         if ((tokens->token >= INPUT && tokens->token <= OUTPUT_A) && !tokens->content)
             return (4);
