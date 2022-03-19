6bb73f3ad5b4c73b1919d796755442c9e3a9a925
Minguk Gaang <minguk.gaang@gmail.com>
2022-03-19 12:58:06 +0100

    Module01-ex02: done

--- 11 C++/Module01/ex02/main.cpp
+++ 11 C++/Module01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: min-kang <minguk.gaang@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 19:18:26 by min-kang          #+#    #+#             */
-/*   Updated: 2022/02/25 20:30:51 by min-kang         ###   ########.fr       */
+/*   Updated: 2022/03/19 12:56:36 by min-kang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,16 @@ int	main(void) {
 	std::string *stringPTR = &str;
 	std::string &stringREF = str;
 
-	std::cout << "==================first exercise=================" << std::endl;
+	std::cout << std::endl << "\033[1;34m** test 1 : showing address of string, string-ptr and string-ref **\033[0m" << std::endl;
 	std::cout << "Address of the string value: " << &str << std::endl;
 	std::cout << "Address displayed using PTR: " << stringPTR << std::endl;
 	std::cout << "Address displayed using REF: " << &stringREF << std::endl;
-	std::cout << "=================second exercise=================" << std::endl;
+
+	std::cout << std::endl << "\033[1;34m** test 2 : showing the string by using string-ptr and string-ref **\033[0m" << std::endl;
 	std::cout << "String displayed using PTR: " << *stringPTR << std::endl;
 	std::cout << "String displayed using REF: " << stringREF << std::endl;
-	std::cout << "=================================================" << std::endl;
+
+	std::cout << std::endl;
 
 	return (0);
 }
