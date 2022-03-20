1bc065a082f78219a236b97c49b1509ad1e87c47
Minguk Gaang <minguk.gaang@gmail.com>
2022-03-20 13:10:40 +0100

    Module01-ex04: Done

--- 11 C++/Module01/ex04/main.cpp
+++ 11 C++/Module01/ex04/main.cpp
@@ -6,39 +6,43 @@
 /*   By: min-kang <minguk.gaang@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 23:08:20 by min-kang          #+#    #+#             */
-/*   Updated: 2022/02/27 16:07:10 by min-kang         ###   ########.fr       */
+/*   Updated: 2022/03/20 13:08:05 by min-kang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 
 int main(void) {
+
 	std::ifstream input;
-	std::ofstream output;
-	std::string s1 = "lol", s2 = "yo";
-	std::string filename = "fuck";
+	std::string s1 = "The Balrog", s2 = "Eric Zemmour";
+	std::string filename = "Gandalf";
 
 	input.open(filename);
 	if (!input) {
 		std::cout << "Cannot read the file" << std::endl;
 		exit(EXIT_FAILURE);
 	}
+
 	std::string r;
-	size_t i;
-	output.open(filename + ".replace");
-	while (getline(input, r)) {
+	r = std::string((std::istreambuf_iterator<char>(input)), std::istreambuf_iterator<char>());
+
+	size_t	i = r.find(s1);
+	while (i != std::string::npos) {
+		r.erase(i, s1.length());
+		r.insert(i, s2);
 		i = r.find(s1);
-		while (i != std::string::npos) {
-			r.erase(i, s1.length());
-			r.insert(i, s2);
-			i = r.find(s1);
-		}
-		output << r << std::endl;
-		// should i have to be worried about \n?
 	}
+
+	std::ofstream output;
+	output.open(filename + ".replace");
+	output << r;
+
 	input.close();
 	output.close();
+
 	return (0);
 }
