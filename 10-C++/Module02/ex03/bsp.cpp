28485c427366895a342ca4bfd69c71b0bbb354dd
Minguk Gaang <minguk.gaang@gmail.com>
2022-03-24 19:53:07 +0100

    Module02-ex03: advanced

--- 10-C++/Module02/ex03/bsp.cpp
+++ 10-C++/Module02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: min-kang <minguk.gaang@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 01:24:48 by min-kang          #+#    #+#             */
-/*   Updated: 2022/03/24 17:25:34 by min-kang         ###   ########.fr       */
+/*   Updated: 2022/03/24 19:52:09 by min-kang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,20 @@
 
 float sign (Point const a, Point const b, Point const c)
 {
-	
-    return (a.getX() - c.getX()) * (b.getY() - c.getY()) - (b.getX() - c.getX()) * (a.getY() - c.getY());
+    return ((a.getX().toFloat() - c.getX().toFloat()) * (b.getY().toFloat() - c.getY().toFloat())
+        - (b.getX().toFloat() - c.getX().toFloat()) * (a.getY().toFloat() - c.getY().toFloat()));
 }
 
 bool	bsp(Point const a, Point const b, Point const c, Point const point) {
-	
-	
+	float d1, d2, d3;
+    bool has_neg, has_pos;
+
+    d1 = sign(point, a, b);
+    d2 = sign(point, b, c);
+    d3 = sign(point, c, a);
+
+    has_neg = (d1 < 0) || (d2 < 0) || (d3 < 0);
+    has_pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
+
+    return !(has_neg && has_pos);
 }
