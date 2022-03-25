ebcaa22b3e17a6103b384978dd76587e3dd3e896
Minguk Gaang <minguk.gaang@gmail.com>
2022-03-26 00:33:49 +0100

    Module03-ex03: now need to make ClapTrap class abstract

--- 10-C++/Module03/ex03/DiamondTrap.cpp
+++ 10-C++/Module03/ex03/DiamondTrap.cpp
@@ -6,7 +6,32 @@
 /*   By: min-kang <minguk.gaang@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 22:22:40 by min-kang          #+#    #+#             */
-/*   Updated: 2022/03/21 22:22:41 by min-kang         ###   ########.fr       */
+/*   Updated: 2022/03/26 00:32:02 by min-kang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "main.hpp"
+
+DiamondTrap::DiamondTrap(void) {
+	
+}
+
+DiamondTrap::DiamondTrap(std::string n) : ClapTrap(n) {
+	
+}
+
+DiamondTrap::DiamondTrap(DiamondTrap const & copy) {
+	
+}
+
+DiamondTrap::~DiamondTrap(void) {
+	std::cout << "Destructor activated" << std::endl;
+}
+
+DiamondTrap& DiamondTrap::operator=(DiamondTrap const & right) {
+	
+}
+
+void	DiamondTrap::whoAmI(void) {
+	
+}
--- 10-C++/Module03/ex03/DiamondTrap.hpp
+++ 10-C++/Module03/ex03/DiamondTrap.hpp
@@ -6,17 +6,25 @@
 /*   By: min-kang <minguk.gaang@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 22:22:38 by min-kang          #+#    #+#             */
-/*   Updated: 2022/03/21 22:26:46 by min-kang         ###   ########.fr       */
+/*   Updated: 2022/03/26 00:27:55 by min-kang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.hpp"
 
-class DiamondTrap : public ClapTrap, public ScavTrap, public FragTrap {
+class DiamondTrap : public ScavTrap, public FragTrap {
 	
 	public:
+		DiamondTrap(void);
+		DiamondTrap(std::string);
+		DiamondTrap(DiamondTrap const &);
+		~DiamondTrap(void);
 
+		DiamondTrap& operator=(DiamondTrap const &);
+
+		void	whoAmI(void);
 
 	private:
 		std::string	_name;
-}
+		
+};
