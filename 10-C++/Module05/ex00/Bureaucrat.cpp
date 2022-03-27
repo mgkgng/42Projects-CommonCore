64c38cfdd7733802e20bb9bf0ccdc8eb1381eab3
Minguk Gaang <minguk.gaang@gmail.com>
2022-03-27 23:21:21 +0200

    Module05-ex00: almost done

--- 10-C++/Module05/ex00/Bureaucrat.cpp
+++ 10-C++/Module05/ex00/Bureaucrat.cpp
@@ -6,27 +6,65 @@
 /*   By: min-kang <minguk.gaang@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 20:01:31 by min-kang          #+#    #+#             */
-/*   Updated: 2022/03/27 20:03:58 by min-kang         ###   ########.fr       */
+/*   Updated: 2022/03/27 23:19:24 by min-kang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat(void) {
+Bureaucrat::Bureaucrat(void) : _name("(default)") {
 	std::cout << "Default Bureaucrat constructor called." << std::endl;
 }
 
-Bureaucrat::Bureaucrat(unsigned int grade) : _grade(grade) {
+Bureaucrat::Bureaucrat(std::string name, unsigned int grade) : _name(name) {
 	std::cout << "Grade Bureaucrat constructor called." << std::endl;
+	if (grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	if (grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+	this->_grade = grade;
 }
 
 Bureaucrat::Bureaucrat(Bureaucrat const & copy) {
-	std::cout << "Copy Bureaucrat cnostructor called." << std::endl;
+	std::cout << "Copy Bureaucrat constructor called." << std::endl;
 	*this = copy;
 }
 
+Bureaucrat::~Bureaucrat(void) {
+	std::cout << "Bureaucrat destructor called." << std::endl;
+}
+
 Bureaucrat& Bureaucrat::operator=(Bureaucrat const & right) {
-	this->_name = right._name;
+	(std::string) this->_name = right._name;
 	this->_grade = right._grade;
 	return (*this);
+}
+
+std::ostream& operator<<(std::ostream& out, Bureaucrat const & one) {
+	out << one.getName() << ", bureaucrat grade " << one.getGrade() << ".";
+	return (out);
+}
+
+std::string	const Bureaucrat::getName(void) const {
+	return (this->_name);
+}
+
+unsigned int Bureaucrat::getGrade(void) const {
+	return (this->_grade);
+}
+
+void	Bureaucrat::UpGrade(void) {
+	if (this->_grade == 1) {
+		throw Bureaucrat::GradeTooHighException();
+		return ;
+	}
+	this->_grade--;
+}
+
+void	Bureaucrat::DownGrade(void) {
+	if (this->_grade == 150) {
+		throw Bureaucrat::GradeTooLowException();
+		return ;
+	}
+	this->_grade++;
 }
--- 10-C++/Module05/ex00/Bureaucrat.hpp
+++ 10-C++/Module05/ex00/Bureaucrat.hpp
@@ -6,12 +6,15 @@
 /*   By: min-kang <minguk.gaang@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 19:58:41 by min-kang          #+#    #+#             */
-/*   Updated: 2022/03/27 20:01:23 by min-kang         ###   ########.fr       */
+/*   Updated: 2022/03/27 23:19:40 by min-kang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
 #include <iostream>
 #include <string>
+#include <stdexcept>
 
 class Bureaucrat {
 	
@@ -21,9 +24,30 @@ class Bureaucrat {
 	
 	public:
 		Bureaucrat(void);
-		Bureaucrat(unsigned int grade);
+		Bureaucrat(std::string name, unsigned int grade);
 		Bureaucrat(Bureaucrat const &);
 		~Bureaucrat(void);
 
 		Bureaucrat& operator=(Bureaucrat const &);
+
+		std::string	const	getName(void) const;
+		unsigned int		getGrade(void) const;
+		void				UpGrade(void);
+		void				DownGrade(void);
+
+		class GradeTooHighException : public std::exception {
+			public:
+				const char *what() const throw() {
+					return ("ERROR: GRADE TOO HIGH");
+				}
+		};
+		
+		class GradeTooLowException : public std::exception {
+			public:
+				const char *what() const throw() {
+					return ("ERROR: GRADE TOO LOW");
+				}
+		};
 };
+
+std::ostream& operator<<(std::ostream& out, Bureaucrat const &);
