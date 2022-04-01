2db4e70703ec63498118bf452c9694d7c52f3764
Minguk Gaang <minguk.gaang@gmail.com>
2022-04-01 03:35:32 +0200

    Module08: started

---
+++ 10-C++/Module08/ex00/easyfind.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <algorithm>
+#include <list>
+#include <map>
+#include <vector>
+
+template<typename T>
+int easyfind(T container, int n) {
+	for_each(container.begin(), container.end(), );
+}
